e395e977450fc8af101276acb1d74a701779adea
reiquel <carelary92@gmail.com>
2025-01-18 12:43:56 -0500

    hjgj

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,16 +6,13 @@ lastModified.textContent = `Last modified: ${document.lastModified}`
 
 
 
-const navigation = document.querySelector('.navigation');
 const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
 const pageTitle = document.querySelector('#page-title');
 
 hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
+    const isOpen = navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
-    if (navigation.classList.contains('open')) {
-        pageTitle.style.display = 'none';
-    } else {
-        pageTitle.style.display = 'block';
-    }
+    hamButton.setAttribute('aria-label', isOpen ? 'Close Menu' : 'Open Menu');
+    pageTitle.style.display = isOpen ? 'none' : 'block';
 });
